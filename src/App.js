c78035ffea4dab03e2acde6b33e893ed6c1553fd
Mike <mprquinn@gmail.com>
2018-02-09 11:50:48 -0700

    toggle slide on scroll

--- src/App.js
+++ src/App.js
@@ -3,10 +3,60 @@ import Project from './components/Project';
 import './styles/project.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [
+        {
+          title: 'Project One',
+          imageUrl: 76,
+          description: 'This is the first project and it has a cool description.'
+        },
+        {
+          title: 'Project Two',
+          imageUrl: 94,
+          description: 'This is the second project. It has a different project.'
+        }
+      ],
+      currentSlide: 0
+    }
+  }
+  changeSlide(dir) {
+    console.log(dir);
+    const currentSlide = this.state.currentSlide;
+    const projects = this.state.projects;
+    if (dir === 'down') {
+      if (currentSlide < projects.length-1) {
+        this.setState({
+          currentSlide: currentSlide+1
+        });
+      }
+    } else {
+      if (currentSlide >  0) {
+        this.setState({
+          currentSlide: currentSlide-1
+        });
+      }
+    }
+  }
+  determineDir(delta) {
+    if (delta > 0) {
+      return 'down';
+    } else {
+      return 'up';
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('mousewheel', (e) => {
+      this.changeSlide(this.determineDir(e.deltaY));
+    });
+  }
   render() {
+    const currSlide = this.state.projects[this.state.currentSlide];
     return (
       <div className="projects">
-        <Project title={`Project One`}/>
+        <Project title={currSlide.title} image={currSlide.imageUrl} description={currSlide.description} />
       </div>
     );
   }
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,14 +5,12 @@ class Project extends Component {
     return (
       <div className="project">
         <div className="project__image-wrap">
-          <img src="//unsplash.it/500/500?image=76" alt="" className="project__image" />
+          <img src={`//unsplash.it/500/500?image=${this.props.image}`} alt="" className="project__image" />
         </div>
         <div className="project__details">
           <h1 className="project__title">{this.props.title}</h1>
           <div className="project__description">
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus maxime rerum atque ratione asperiores possimus fugiat cumque, minus at eligendi quibusdam, quas consectetur explicabo? Unde expedita nesciunt reiciendis nostrum doloremque?
-            </p>
+            {this.props.description}
           </div>
         </div>
       </div>
