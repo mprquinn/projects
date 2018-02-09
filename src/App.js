fe279d62a300414164b96858f6e928a797fc77c1
Mike <mprquinn@gmail.com>
2018-02-09 15:46:42 -0700

    reference local images

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import React, { Component } from 'react';
-import Project from './components/Project';
-import './styles/project.css';
+import React, { Component } from "react";
+import Project from "./components/Project";
+import "./styles/project.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      loaded: 'loading',
+      loaded: "loaded",
       animating: false,
-      animateDir: '',
+      animateDir: "",
       animateDuration: 600,
       projects: [
         {
-          title: 'Project One',
-          imageUrl: 76,
-          description: 'This is the first project and it has a cool description.'
+          title: "Project One",
+          imageUrl: "project1.jpg",
+          description:
+            "This is the first project and it has a cool description."
         },
         {
-          title: 'Project Two',
-          imageUrl: 94,
-          description: 'This is the second project. It has a different project.'
+          title: "Project Two",
+          imageUrl: "project2.jpg",
+          description: "This is the second project. It has a different project."
         }
       ],
       currentSlide: 0
-    }
+    };
   }
   fireAnims(duration) {
     // go
     this.setState({
       animating: true,
-      animateDir: 'out'
+      animateDir: "out"
     });
     // halfway
     window.setTimeout(() => {
       this.setState({
-        animateDir: 'in'
+        animateDir: "in"
       });
-    }, duration/2);
+    }, duration / 2);
     // done
     window.setTimeout(() => {
       this.setState({
@@ -49,22 +50,21 @@ class App extends Component {
     const currentSlide = this.state.currentSlide;
     const projects = this.state.projects;
 
-    if (dir === 'down') {
-      if (currentSlide < projects.length-1) {
+    if (dir === "down") {
+      if (currentSlide < projects.length - 1) {
         this.fireAnims(1200);
         window.setTimeout(() => {
           this.setState({
-            currentSlide: currentSlide+1,
+            currentSlide: currentSlide + 1
           });
         }, 610);
-        
       }
     } else {
-      if (currentSlide >  0) {
+      if (currentSlide > 0) {
         this.fireAnims(1200);
         window.setTimeout(() => {
           this.setState({
-            currentSlide: currentSlide-1,
+            currentSlide: currentSlide - 1
           });
         }, 610);
       }
@@ -72,38 +72,48 @@ class App extends Component {
   }
   determineDir(delta) {
     if (delta > 0) {
-      return 'down';
+      return "down";
     } else {
-      return 'up';
+      return "up";
     }
   }
   loadImages() {
     const projects = this.state.projects;
-    const baseUrl = '//unsplash.it/500x500?image=';
+    const baseUrl = "//unsplash.it/500x500?image=";
     projects.forEach((project, i) => {
       let image = new Image();
       const url = `${baseUrl}${project.imageUrl}`;
       image.src = url;
       if (i === projects.length - 1) {
-        this.setState({
-          loaded: 'loaded'
-        });
+        // this.setState({
+        //   loaded: 'loaded'
+        // });
       }
     });
   }
   componentWillMount() {
-    this.loadImages();
+    // this.loadImages();
   }
   componentDidMount() {
-    document.addEventListener('mousewheel', (e) => {
-      this.changeSlide(this.determineDir(e.deltaY));      
+    document.addEventListener("mousewheel", e => {
+      this.changeSlide(this.determineDir(e.deltaY));
     });
   }
   render() {
     const currSlide = this.state.projects[this.state.currentSlide];
+    const currentImage = require(`./${currSlide.imageUrl}`);
     return (
-      <div className={`projects projects--${this.state.loaded}`} style={{backgroundImage: `url(https://unsplash.it/500x500?image=${currSlide.imageUrl})` }}>
-        <Project title={currSlide.title} image={currSlide.imageUrl} description={currSlide.description} animClass={this.state.animateDir} animating={this.state.animating} />
+      <div
+        className={`projects projects--${this.state.loaded}`}
+        style={{ backgroundImage: `url(${currentImage})` }}
+      >
+        <Project
+          title={currSlide.title}
+          image={currSlide.imageUrl}
+          description={currSlide.description}
+          animClass={this.state.animateDir}
+          animating={this.state.animating}
+        />
       </div>
     );
   }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Project extends Component {
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
   render() {
-    let classes = [
-      'project',
-    ];
+    let classes = ["project"];
     if (this.props.animating) {
-      classes.push(`project--animated project--${this.props.animClass}`)
+      classes.push(`project--animated project--${this.props.animClass}`);
     } else {
-      classes = [
-        'project'
-      ];
+      classes = ["project"];
     }
     return (
-      <div className={classes.join(' ')}>
+      <div className={classes.join(" ")}>
         <div className="project__image-wrap">
-          <img src={`//unsplash.it/500/500?image=${this.props.image}`} alt="" className={`project__image project__image${this.props.image}`} />
+          <img
+            src={require(`../${this.props.image}`)}
+            alt=""
+            className={`project__image project__image${this.props.image}`}
+          />
         </div>
         <div className="project__details">
           <h1 className="project__title">{this.props.title}</h1>
-          <div className="project__description">
-            {this.props.description}
-          </div>
+          <div className="project__description">{this.props.description}</div>
         </div>
       </div>
     );
