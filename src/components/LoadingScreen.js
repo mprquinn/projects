47bdfc1efa2287b6eaa8951bdbcc092d423e9c8b
Mike Quinn <Ask>
2018-02-10 13:30:46 -0700

    initial loading screen component

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LoadingScreen extends Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="loading-screen">
+        <h1>Loading</h1>
+      </div>
+    );
+  }
+}
+
+export default LoadingScreen;
